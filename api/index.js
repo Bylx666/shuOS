2cdc00360247e6ef826acb3b35c50b08b856fd2e
Subkey <2822448396@qq.com>
2022-01-22 12:59:38 +0800

    test

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 var port = process.env.PORT || 8088
 var host = process.env.HOSTNAME || 'localhost'
 http.createServer((req,res)=>{
-  let pathName = path.join(__dirname,'index.html')
+  let pathName = path.join(__dirname,'home.html')
   let page = fs.readFileSync(pathName)
   res.write(page)
   res.end()
